94a78fc1db1a0f31c84bb74731e8720b19ef3e8b
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-04-06 09:55:15 -0700

    update dhcpd.conf, checking first byte in mac address, which is '02' for roach2  mac address.

--- netboot_env.sh
+++ netboot_env.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-export TFTP_DIR=/home/wei/roach2_netboot/tftp
-export NFS_SHARE_DIR=/home/wei/roach2_netboot/nfs/current
+export TFTP_DIR=//home/wei/roach2_nfs_uboot/boot
+export NFS_SHARE_DIR=/home/wei/roach2_nfs_uboot/debian_stable_devel
 export DHCP_PORT=enp6s0
