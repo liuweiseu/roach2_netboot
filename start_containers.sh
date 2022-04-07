56335f27204f750066128907d4d6ee2c5ffaae1a
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-04-07 11:48:26 -0700

    add scripts for start and stop containers.

---
+++ start_containers.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo docker start r2-dhcp
+sudo docker start r2-tftp
+sudo docker start r2-nfs-v3
---
+++ stop_containers.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo docker stop r2-dhcp
+sudo docker stop r2-tftp
+sudo docker stop r2-nfs-v3
